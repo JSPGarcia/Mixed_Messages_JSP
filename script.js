74965c21a7c6421a9341feb7319ad27cf070d8c2
Gralgssf9 <jenner@jenners-air.home>
2021-04-22 17:10:54 -0400

    script halfway there!

---
+++ script.js
@@ -0,0 +1,15 @@
+//Array Hub:
+const Phrases = [
+    "The bewildered tourist was lost",
+    "The lost puppy was a wet and stinky dog.",
+    "The flu clinic had seen many cases of infectious disease.",
+    "It was a story as old as time.",
+    "The sports car drove the long and winding road.",
+    "Saturday became a cool, wet afternoon.",
+    "He was waiting for the rain to stop.",
+];
+//variable Hub:
+let phrase = Phrases[Math.floor(Math.random() * Phrases.length)]
+
+alert(phrase)
+    //Function bay
